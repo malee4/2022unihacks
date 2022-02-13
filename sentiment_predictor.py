d224e56e187c9715eafff7369003995f12730fdf
malee4 <55629914+malee4@users.noreply.github.com>
2022-02-13 16:28:38 -0500

    Create sentiment_predictor.py

---
+++ sentiment_predictor.py
@@ -0,0 +1,188 @@
+import sys
+import os
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+from IPython.display import clear_output
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+% matplotlib inline
+
+from os import path
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS
+
+!pip install --upgrade git+https://github.com/flairNLP/flair.git
+
+clear_output()
+
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+tagger = SequenceTagger.load('ner')
+
+clear_output()
+
+from flair.models import TextClassifier
+
+classifier = TextClassifier.load('en-sentiment')
+
+clear_output()
+
+
+#AUTH need KEYS HERE
+auth = tweepy.AppAuthHandler(TWITTER_KEY, TWITTER_SECRET_KEY)
+
+api = tweepy.API(auth, wait_on_rate_limit=True,
+				   wait_on_rate_limit_notify=True)
+
+if (not api):
+    print ("Can't Authenticate")
+    sys.exit(-1)
+
+#@title Twitter Search API Inputs
+#@markdown ### Enter Search Query:
+searchQuery = 'Seattle' #@param {type:"string"}
+#@markdown ### Enter Max Tweets To Scrape:
+#@markdown #### The Twitter API Rate Limit (currently) is 45,000 tweets every 15 minutes.
+maxTweets = 1000 #@param {type:"slider", min:0, max:45000, step:100}
+Filter_Retweets = True #@param {type:"boolean"}
+
+tweetsPerQry = 100  # this is the max the API permits
+tweet_lst = []
+
+if Filter_Retweets:
+  searchQuery = searchQuery + ' -filter:retweets'  # to exclude retweets
+
+sinceId = None
+
+
+max_id = -10000000000
+
+tweetCount = 0
+print("Downloading max {0} tweets".format(maxTweets))
+while tweetCount < maxTweets:
+    try:
+        if (max_id <= 0):
+            if (not sinceId):
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry, lang="en")
+            else:
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        lang="en", since_id=sinceId)
+        else:
+            if (not sinceId):
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        lang="en", max_id=str(max_id - 1))
+            else:
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        lang="en", max_id=str(max_id - 1),
+                                        since_id=sinceId)
+        if not new_tweets:
+            print("No more tweets found")
+            break
+        for tweet in new_tweets:
+          if hasattr(tweet, 'reply_count'):
+            reply_count = tweet.reply_count
+          else:
+            reply_count = 0
+          if hasattr(tweet, 'retweeted'):
+            retweeted = tweet.retweeted
+          else:
+            retweeted = "NA"
+            
+          # fixup search query to get topic
+          topic = searchQuery[:searchQuery.find('-')].capitalize().strip()
+          
+          # fixup date
+          tweetDate = tweet.created_at.date()
+          
+          tweet_lst.append([tweetDate, topic, 
+                      tweet.id, tweet.user.screen_name, tweet.user.name, tweet.text, tweet.favorite_count, 
+                      reply_count, tweet.retweet_count, retweeted])
+
+        tweetCount += len(new_tweets)
+        print("Downloaded {0} tweets".format(tweetCount))
+        max_id = new_tweets[-1].id
+    except tweepy.TweepError as e:
+        # Just exit if any error
+        print("some error : " + str(e))
+        break
+
+clear_output()
+print("Downloaded {0} tweets".format(tweetCount))
+
+pd.set_option('display.max_colwidth', -1)
+
+# load it into a pandas dataframe
+tweet_df = pd.DataFrame(tweet_lst, columns=['tweet_dt', 'topic', 'id', 'username', 'name', 'tweet', 'like_count', 'reply_count', 'retweet_count', 'retweeted'])
+tweet_df.to_csv('tweets.csv')
+tweet_df.head()
+
+#@title Filter By Date Range
+today = datetime.now().date()
+yesterday = today - timedelta(1)
+
+start_dt = '' #@param {type:"date"}
+end_dt = '' #@param {type:"date"}
+
+if start_dt == '':
+  start_dt = yesterday
+else:
+  start_dt = datetime.strptime(start_dt, '%Y-%m-%d').date()
+
+if end_dt == '':
+  end_dt = today
+else:
+  end_dt = datetime.strptime(end_dt, '%Y-%m-%d').date()
+
+
+tweet_df = tweet_df[(tweet_df['tweet_dt'] >= start_dt) 
+                    & (tweet_df['tweet_dt'] <= end_dt)]
+tweet_df.shape
+
+# predict NER
+nerlst = []
+
+for index, row in tqdm(tweet_df.iterrows(), total=tweet_df.shape[0]):
+  cleanedTweet = row['tweet'].replace("#", "")
+  sentence = Sentence(cleanedTweet, use_tokenizer=True)
+  
+  # predict NER tags
+  tagger.predict(sentence)
+
+  # get ner
+  ners = sentence.to_dict(tag_type='ner')['entities']
+  
+  # predict sentiment
+  classifier.predict(sentence)
+  
+  label = sentence.labels[0]
+  response = {'result': label.value, 'polarity':label.score}
+  
+  # get hashtags
+  hashtags = re.findall(r'#\w+', row['tweet'])
+  if len(hashtags) >= 1:
+    for hashtag in hashtags:
+      ners.append({ 'type': 'Hashtag', 'text': hashtag })
+  
+  for ner in ners:
+    adj_polarity = response['polarity']
+    if response['result'] == 'NEGATIVE':
+      adj_polarity = response['polarity'] * -1
+    try:
+      ner['type']
+    except:
+      ner['type'] = ''      
+    nerlst.append([ row['tweet_dt'], row['topic'], row['id'], row['username'], 
+                   row['name'], row['tweet'], ner['type'], ner['text'], response['result'], 
+                   response['polarity'], adj_polarity, row['like_count'], row['reply_count'], 
+                  row['retweet_count'] ])
+
+clear_output()
